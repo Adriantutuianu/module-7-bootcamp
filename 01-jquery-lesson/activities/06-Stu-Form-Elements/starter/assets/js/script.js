9e8741a3d9f405e247677bbc6410fb345ab1e1a2
Adrian Tut <adriantutuianu01@gmail.com>
2024-01-02 20:52:02 +0000

    Jquery task 3

--- 01-jquery-lesson/activities/06-Stu-Form-Elements/starter/assets/js/script.js
+++ 01-jquery-lesson/activities/06-Stu-Form-Elements/starter/assets/js/script.js
@@ -1,6 +1,22 @@
-const shoppingFormEl = $('#shopping-form');
-const shoppingListEl = $('#shopping-list');
+const shoppingFormEl = $("#shopping-form");
+const shoppingListEl = $("#shopping-list");
 
 // TODO: Create a function to handle the form submission event that captures the form's `<input>` value and prints it to the `shoppingListEl` as a `<li>`
 
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  const shoppingItem = $("input[name=shopping-input]").val();
+
+  if (!shoppingItem) {
+    console.log("no shopping intem filled out in the form!");
+    return;
+  }
+  // print to the page
+
+  shoppingListEl.append("<li>" + shoppingItem + "</li>");
+  //clear the form
+  $('input[name="shopping-input"]').val("");
+}
 // TODO: Add an event listener to the `shoppingFormEl` to handle submission
+shoppingFormEl.on("submit", handleFormSubmit);
