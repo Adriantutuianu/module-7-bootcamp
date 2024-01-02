6355d0f6a8f31114bf29ac35b60bacbfc97fd22e
Adrian Tut <adriantutuianu01@gmail.com>
2024-01-02 21:20:52 +0000

    Jquery task 4

--- 01-jquery-lesson/activities/08-Stu-Dom-Traversal/starter/assets/js/script.js
+++ 01-jquery-lesson/activities/08-Stu-Dom-Traversal/starter/assets/js/script.js
@@ -1,6 +1,9 @@
 // Assemble: Create/select DOM elements
-const rootEl = $('#root');
+const rootEl = $("#root");
 
 // TODO: Starting from the root element, `rootEl`, make all the background color for each box white.
-
+rootEl.children("ul").children().css("background-color", "white");
 // TODO: Starting from `rootEl`, create the jQuery statement that will add "O" to block the "X" from winning
+
+const rowThree = rootEl.children("ul").eq(2);
+rowThree.children().eq(0).text("O");
