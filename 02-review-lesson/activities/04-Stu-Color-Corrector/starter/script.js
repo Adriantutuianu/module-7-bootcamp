06e9da5c8bce05e979fdba61d73ea3f76d27e2ba
Adrian Tut <adriantutuianu01@gmail.com>
2024-01-03 23:38:17 +0000

    Color corrector activity

--- 02-review-lesson/activities/04-Stu-Color-Corrector/starter/script.js
+++ 02-review-lesson/activities/04-Stu-Color-Corrector/starter/script.js
@@ -18,31 +18,67 @@ $(document).ready(function () {
   // This function sets a random color "word" and a random set of colors "visuals"
   function randColor() {
     // Set random word
-    var word = Math.floor(Math.random() * colors.length);
+    targetColor = colors[Math.floor(Math.random() * colors.length)];
+    $("#rand-target-color").text(targetColor);
     // Set random word color
-    var randomWord = colors[word];
-    console.log(randomWord);
+    targetColorOfText = colors[Math.floor(Math.random() * colors.length)];
+    $("#rand-target-color").css("color", targetColorOfText);
   }
 
   // This function creates the actual "game" logic.
   function createColorPicker() {
     // Shuffle color array
+    colors.sort(function () {
+      return 0.5 - Math.random();
+    });
     // Push to text color array
+    for (let i = 0; i < colors.length; i++) {
+      colorsOfText.push(colors[i]);
+    }
     // Shuffle text color array
+    colorsOfText.sort(function () {
+      return 0.5 - Math.random();
+    });
+
     // Loop through all colors in the array
-    // Create element to hold word
-    // Output a word
-    // Make word a random color
+    for (let i = 0; i < colors.length; i++) {
+      // Create element to hold word
+      var holder = document.createElement("li");
+      $("#color-picker").append(holder);
+
+      // Output a word
+      $(holder).attr("id", colors[i]).text(colors[i]);
+      $(holder).css("color", colorsOfText[i]);
+      // Make word a random color
+    }
   }
 
   // Clear the divs and arrays upon each round.
-  function clear() {}
+  function clear() {
+    $("#rand-target-color").empty();
+    $("#color-picker").empty();
+    colorsOfText = [];
+  }
 
   // Function for resetting colors and setting new colors.
-  function reset() {}
+  function reset() {
+    clear();
+    randColor();
+    createColorPicker();
+  }
 
   // Get id of element clicked by user
-  $("#color-picker").click(function (event) {});
+  $("#color-picker").click(function (event) {
+    userAnswer = event.target.id;
+
+    if (userAnswer === targetColorOfText) {
+      alert("You win!");
+      reset();
+    } else {
+      alert("Wrong!");
+      reset();
+    }
+  });
 
   // Run game
   reset();
