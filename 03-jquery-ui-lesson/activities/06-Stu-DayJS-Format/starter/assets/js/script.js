14559c4765dda24b2dba480e6ecdabfc445c1118
Adrian Tut <adriantutuianu01@gmail.com>
2024-01-04 20:13:54 +0000

    Dayjs format

--- 03-jquery-ui-lesson/activities/06-Stu-DayJS-Format/starter/assets/js/script.js
+++ 03-jquery-ui-lesson/activities/06-Stu-DayJS-Format/starter/assets/js/script.js
@@ -1,25 +1,28 @@
 // Use Day.js.js to format the date and assign to the declared variable.
 // TODO: 1. What is your graduation date in the following format: Jan 1st, 1999?
-const gradDate;
+const gradDate = dayjs("2024-03-08").format("MMM D, YYYY");
 $("#1a").text(gradDate);
 
 // TODO: 2. What day of the week will 1/1/2027 be?
-const weekDay;
+var weekDay = dayjs(" 1/1/2027").format("dddd, MMMM D, YYYY");
 $("#2a").text(weekDay);
 
 // TODO: 3. What is the difference in days between May 4, 2027 and today?
-const days;
-$("#3a").text(now);
+var today = dayjs();
+
+var targetDay = dayjs("2027-05-04");
+
+var days = targetDay.diff(today, "day");
+$("#3a").text(days);
 
 // TODO: 4. What is the current time in the format: hours:minutes:seconds
-const time;
+var time = dayjs().format("HH:mm:ss");
 $("#4a").text(time);
 
 // TODO: 5. What is the current Unix timestamp?
-const unix;
+var unix = dayjs().unix();
 $("#5a").text(unix);
 
 // TODO: 6. Parse the following Unix timestamp, 1318781876, and convert into any time/date format.
-const unixFormat;
+var unixFormat = dayjs.unix(1318781876).format("D MMM YYYY, HH:mm:ss");
 $("#6a").text(unixFormat);
-
