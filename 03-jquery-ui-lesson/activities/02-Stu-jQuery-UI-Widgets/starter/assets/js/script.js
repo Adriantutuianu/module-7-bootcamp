8f9714246d058877ee7f4513b87511f9af6a8864
Adrian Tut <adriantutuianu01@gmail.com>
2024-01-04 19:07:16 +0000

    Jquiery ui widgets

--- 03-jquery-ui-lesson/activities/02-Stu-jQuery-UI-Widgets/starter/assets/js/script.js
+++ 03-jquery-ui-lesson/activities/02-Stu-jQuery-UI-Widgets/starter/assets/js/script.js
@@ -1,12 +1,12 @@
-const formEl = $('#skills-form');
-const nameInputEl = $('#skill-name');
-const dateInputEl = $('#datepicker');
-const skillsListEl = $('#skills-list');
+const formEl = $("#skills-form");
+const nameInputEl = $("#skill-name");
+const dateInputEl = $("#datepicker");
+const skillsListEl = $("#skills-list");
 
 const printSkills = function (name, date) {
-  const listEl = $('<li>');
-  const listDetail = name.concat(' on ', date);
-  listEl.addClass('list-group-item').text(listDetail);
+  const listEl = $("<li>");
+  const listDetail = name.concat(" on ", date);
+  listEl.addClass("list-group-item").text(listDetail);
   listEl.appendTo(skillsListEl);
 };
 
@@ -17,20 +17,51 @@ const handleFormSubmit = function (event) {
   const dateInput = dateInputEl.val();
 
   if (!nameInput || !dateInput) {
-    console.log('You need to fill out the form!');
+    console.log("You need to fill out the form!");
     return;
   }
 
   printSkills(nameInput, dateInput);
 
-  nameInputEl.val('');
-  dateInputEl.val('');
+  nameInputEl.val("");
+  dateInputEl.val("");
 };
 
-formEl.on('submit', handleFormSubmit);
+formEl.on("submit", handleFormSubmit);
 
 // Add Autocomplete widget here
 //
-
+$(function () {
+  var availableTags = [
+    "ActionScript",
+    "AppleScript",
+    "Asp",
+    "BASIC",
+    "C",
+    "C++",
+    "Clojure",
+    "COBOL",
+    "ColdFusion",
+    "Erlang",
+    "Fortran",
+    "Groovy",
+    "Haskell",
+    "Java",
+    "JavaScript",
+    "Lisp",
+    "Perl",
+    "PHP",
+    "Python",
+    "Ruby",
+    "Scala",
+    "Scheme",
+  ];
+  $("#skill-name").autocomplete({
+    source: availableTags,
+  });
+});
 // Add Datepicker widget here
 //
+$(function () {
+  $("#datepicker").datepicker();
+});
