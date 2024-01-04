54a25ccc69ffa268d4c150699b794cd07b24811a
Adrian Tut <adriantutuianu01@gmail.com>
2024-01-04 19:37:08 +0000

    Jquery ui interaction

--- 03-jquery-ui-lesson/activities/04-Stu-jQuery-UI-Interactions/starter/assets/js/script.js
+++ 03-jquery-ui-lesson/activities/04-Stu-jQuery-UI-Interactions/starter/assets/js/script.js
@@ -1,12 +1,12 @@
-const formEl = $('#skills-form');
-const nameInputEl = $('#skill-name');
-const dateInputEl = $('#datepicker');
-const skillsListEl = $('#skills-list');
+const formEl = $("#skills-form");
+const nameInputEl = $("#skill-name");
+const dateInputEl = $("#datepicker");
+const skillsListEl = $("#skills-list");
 
 const printSkills = function (name, date) {
-  const listEl = $('<li>');
-  const listDetail = name.concat(' on ', date);
-  listEl.addClass('list-group-item').text(listDetail);
+  const listEl = $("<li>");
+  const listDetail = name.concat(" on ", date);
+  listEl.addClass("list-group-item").text(listDetail);
   listEl.appendTo(skillsListEl);
 };
 
@@ -17,53 +17,56 @@ const handleFormSubmit = function (event) {
   const dateInput = dateInputEl.val();
 
   if (!nameInput || !dateInput) {
-    console.log('You need to fill out the form!');
+    console.log("You need to fill out the form!");
     return;
   }
 
   printSkills(nameInput, dateInput);
 
-  nameInputEl.val('');
-  dateInputEl.val('');
+  nameInputEl.val("");
+  dateInputEl.val("");
 };
 
-formEl.on('submit', handleFormSubmit);
+formEl.on("submit", handleFormSubmit);
 
 // Autocomplete widget
 $(function () {
   const skillNames = [
-    'Bootstrap',
-    'C',
-    'C++',
-    'CSS',
-    'Express.js',
-    'Git',
-    'HTML',
-    'Java',
-    'JavaScript',
-    'jQuery',
-    'JSON',
-    'MySQL',
-    'Node.js',
-    'NoSQL',
-    'PHP',
-    'Python',
-    'React',
-    'Ruby',
+    "Bootstrap",
+    "C",
+    "C++",
+    "CSS",
+    "Express.js",
+    "Git",
+    "HTML",
+    "Java",
+    "JavaScript",
+    "jQuery",
+    "JSON",
+    "MySQL",
+    "Node.js",
+    "NoSQL",
+    "PHP",
+    "Python",
+    "React",
+    "Ruby",
   ];
-  $('#skill-name').autocomplete({
+  $("#skill-name").autocomplete({
     source: skillNames,
   });
 });
 
 // Datepicker widget
 $(function () {
-  $('#datepicker').datepicker({
+  $("#datepicker").datepicker({
     changeMonth: true,
     changeYear: true,
-    dateFormat: 'dd/mm/y',
+    dateFormat: "dd/mm/y",
   });
 });
 
 // Add interaction here
 //
+$(function () {
+  $("#skills-list").sortable();
+});
